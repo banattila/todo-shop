f6939c67df2c1764d64e46e9da6944cd9b2be83f
Bán Attila <banattila@protonmail.com>
2022-07-24 16:21:04 +0200

    Finished header and css-s moving and separating to style dir

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from '../assets/note.png';
 import { links } from "../utils/constants";
+import '../styles/header.css';
 
 const Header = () => {
 
@@ -9,13 +10,13 @@ const Header = () => {
 
     return (
         <header>
-            <div>
+            <div className="header-title">
+            <img className="logo" src={logo} alt="logo"/>
                 <h4>Listák</h4>
-                <img src={logo} alt="logo"/>
             </div>
             <nav>
                 {links.map((item, index) => {
-                    return <Link key={index} to={item.to}>{item.title}</Link>
+                    return <Link key={index} className="links" to={item.to}>{item.title}</Link>
                 })}
             </nav>
         </header>
